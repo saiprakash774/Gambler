5b5e8937738fc133cbe8517c4e24746dc6823156
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-12-03 10:23:43 +0530

    Play Game method

--- gambler/Gambler.java
+++ gambler/Gambler.java
@@ -2,7 +2,21 @@ package gambler;
 
 
 public class Gambler {
-
-	private final static int bet=1;
-	private int stake=100;
+	private final static int BET=1;
+	private static int STAKE=100;
+	
+	public static void game() {
+		int result=(int)Math.floor(Math.random()*10)% 1;
+		if(result==1) {
+			STAKE++;
+			System.out.println(STAKE);
+		}
+		else {
+			STAKE--;
+			System.out.println(STAKE);
+		}
+	}
+	public static void main(String args[]) {
+		game();
+	}
 }
